9828129366de9d686820f3488d75be0371ebbb17
flaviusburca <flavius.burca@gmail.com>
2025-03-18 06:51:29 +0200

    - remove obsolete changes

--- core/vm/interpreter.go
+++ core/vm/interpreter.go
@@ -205,7 +205,7 @@ func (in *EVMInterpreter) Run(contract *Contract, input []byte, readOnly bool) (
 		} else if sLen > operation.maxStack {
 			return nil, &ErrStackOverflow{stackLen: sLen, limit: operation.maxStack}
 		}
-		if !contract.UseGas(cost) {
+		if in.evm.meterGas && !contract.UseGas(cost) {
 			return nil, ErrOutOfGas
 		}
 		if operation.dynamicGas != nil {
@@ -226,7 +226,7 @@ func (in *EVMInterpreter) Run(contract *Contract, input []byte, readOnly bool) (
 					return nil, ErrGasUintOverflow
 				}
 			}
-			if operation.dynamicGas != nil {
+			if in.evm.meterGas && operation.dynamicGas != nil {
 				// Consume the gas and return an error if not enough gas is available.
 				// cost is explicitly set so that the capture state defer method can get the proper cost
 				var dynamicCost uint64
